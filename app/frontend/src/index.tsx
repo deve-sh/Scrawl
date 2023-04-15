dc8537d22e1ddf1a801f8413126bbbe5b01741c9
Devesh Kumar <devesh2027@gmail.com>
2023-04-15 17:32:20 +0530

    frontend: add react-router and draft.js

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
 const root = document.getElementById("root") as HTMLElement;
 const rootForApp = createRoot(root);
 
-rootForApp.render(<App />);
+rootForApp.render(
+	<Router>
+		<App />
+	</Router>
+);
