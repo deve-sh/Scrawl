6500ec1092b106465ee5852f8a07da5bb7c552b0
Devesh Kumar <devesh2027@gmail.com>
2023-04-15 14:47:05 +0530

    app: Setup electron boilerplate

---
+++ app/electron/index.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from "electron";
+import isDev from "electron-is-dev";
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+
+	win.loadURL(isDev ? "http://localhost:3000" : "about:blank");
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
